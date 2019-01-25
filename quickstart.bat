ad2a8c19c3ab852e195862fe9e86fb48d832aa62
Pallavi123455 <45254572+Pallavi123455@users.noreply.github.com>
2019-01-25 15:50:49 +0530

    Add files via upload

---
+++ quickstart.bat
@@ -0,0 +1,5 @@
+mvn archetype:generate ^
+-DarchetypeArtifactId=maven-archetype-quickstart ^
+-DgroupId=com.java ^
+-DartifactId=S ^
+-DinteractiveMode=false
---
+++ webapp.bat
@@ -0,0 +1,5 @@
+mvn archetype:generate ^
+-DarchetypeArtifactId=maven-archetype-webapp ^
+-DgroupId=com.java ^
+-DartifactId=Spring2 ^
+-DinteractiveMode=false
